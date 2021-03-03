9508a73b5cb9bfb089afbcd665817e2d90447f6a
Mark Stralka <mark@mobilelocker.com>
2021-03-03 12:19:36 -0500

    MLE-56 added debugging

--- pdfjs/web/mobilelocker.js
+++ pdfjs/web/mobilelocker.js
@@ -1,18 +1,28 @@
 // MLE-56
 (function () {
-  const uri = new URI(window.location.href)
-  const parameters = uri.search(true)
-  if (parameters && parameters.can_be_printed_by_user === '1') {
+  try {
+    const uri = new URI(window.location.href)
+    const parameters = uri.search(true)
+    if (parameters && parameters.can_be_printed_by_user === '1') {
+      console.log('The PDF can be printed');
+    } else {
+      console.log('The PDF cannot be printed');
+      // @see https://developer.mozilla.org/en-US/docs/Web/API/ElementCSSInlineStyle/style
+      let print = document.getElementById('print')
+      if (print) {
+        print.style.display = 'none'
+      } else {
+        console.warn('The print button was not found');
+      }
 
-  } else {
-    let print = document.getElementById('print')
-    if (print) {
-      print.style.display = 'none !important;'
+      let secondaryPrint = document.getElementById('secondaryPrint')
+      if (secondaryPrint) {
+        secondaryPrint.style.display = 'none'
+      } else {
+        console.warn('The secondaryPrint button was not found');
+      }
     }
-
-    let secondaryPrint = document.getElementById('secondaryPrint')
-    if (secondaryPrint) {
-      secondaryPrint.style.display = 'none !important;'
-    }
+  } catch (e) {
+    console.error('MLE-56 exception: ', e.message);
   }
 })()
