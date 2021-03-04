663fc5fa5bbdf61cfcaa9f407b53f9a4d3b2ee21
Mark Stralka <mark@mobilelocker.com>
2021-03-04 10:42:44 -0500

    MLE-56 decode the presentation's base64-encoded parameters and hide the PRINT button if necessary.

--- pdfjs/web/mobilelocker.js
+++ pdfjs/web/mobilelocker.js
@@ -1,26 +1,37 @@
 // MLE-56
 (function () {
+  // @see https://developer.mozilla.org/en-US/docs/Web/API/ElementCSSInlineStyle/style
+  function hideElement(id) {
+    const elm = document.getElementById(id);
+    if (elm) {
+      elm.style.display = 'none';
+    } else {
+      console.warn(`The ${id} element was not found.`);
+    }
+  }
+
+  function showElement(id) {
+    const elm = document.getElementById(id);
+    if (elm) {
+      elm.style.display = null;
+    } else {
+      console.warn(`The ${id} element was not found.`);
+    }
+  }
+
   try {
-    const uri = new URI(window.location.href)
-    const parameters = uri.search(true)
-    if (parameters && parameters.can_be_printed_by_user === '1') {
+    const fullURI = new URI(window.location.href);
+    const fullParams = fullURI.search(true);
+    const file = fullParams.file;
+    const uri = new URI(file);
+    const parameters = uri.search(true);
+    const data = URI.parseQuery(atob(parameters.data));
+    if (data && ['TRUE', 'true', '1', 1, 'YES', 'yes'].includes(data.can_be_printed_by_user)) {
       console.log('The PDF can be printed');
     } else {
-      console.log('The PDF cannot be printed');
-      // @see https://developer.mozilla.org/en-US/docs/Web/API/ElementCSSInlineStyle/style
-      let print = document.getElementById('print')
-      if (print) {
-        print.style.display = 'none'
-      } else {
-        console.warn('The print button was not found');
-      }
-
-      let secondaryPrint = document.getElementById('secondaryPrint')
-      if (secondaryPrint) {
-        secondaryPrint.style.display = 'none'
-      } else {
-        console.warn('The secondaryPrint button was not found');
-      }
+      console.log('The PDF cannot be printed so the PRINT button will be hidden');
+      hideElement('print');
+      hideElement('secondaryPrint');
     }
   } catch (e) {
     console.error('MLE-56 exception: ', e.message);
